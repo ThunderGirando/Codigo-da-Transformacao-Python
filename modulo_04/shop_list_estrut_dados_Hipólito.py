0aa88b8f3ea71a6dd218f5f5d680229ce1b87a43
Hipolito <ghipolitomonteiro@gmail.com>
2025-11-01 11:54:09 -0300

    Create shop_list_estrut_dados_Hipólito.py

---
+++ modulo_04/shop_list_estrut_dados_Hipólito.py
@@ -0,0 +1,14 @@
+"""
+
+
+
+
+
+"""
+
+
+nome = str(input("Qual seu nome: "))
+products = ["Gabinete 1", "Gabinete 2", "Rtx 2060", "Rtx 3060", "Monitor"]
+print(f"Escolha um Produto:\n\n{products}")
+escolha = int(input())
+print(products[escolha])
